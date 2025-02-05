74643e55af3ee66ec9c946599c64dc48f01a8038
Lukas Matuska <lukas.matuska@tescan.com>
2025-02-05 17:30:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mattermost Activity manager
+
+This is repository for Docker container to easily and periodicaly change MM status.
---
+++ src/main.py
@@ -0,0 +1,83 @@
+"""docstring"""
+from datetime import datetime
+import os
+import requests
+# import time
+# import threading
+
+MM_URL = os.getenv("MM_URL", "https://your-mattermost-instance.com")
+MM_TOKEN = os.getenv("MM_TOKEN", "YOUR_ACCESS_TOKEN")
+MM_USER_ID = os.getenv("MM_USER_ID", "YOUR_USER_ID")
+REQUESTS_TIMEOUT = float(os.getenv("REQUESTS_TIMEOUT", "10"))
+
+headers = {
+    "Authorization": f"Bearer {MM_TOKEN}",
+    "Content-Type": "application/json"
+}
+
+
+def send_request(
+    url: str | bytes,
+    payload,
+):
+    """
+    Function for sending request to MM REST API
+    """
+    response = requests.put(
+        url,
+        json=payload,
+        headers=headers,
+        timeout=REQUESTS_TIMEOUT
+    )
+
+    if response.status_code == 200:
+        print(f"Status successfully set at {datetime.now()}")
+    else:
+        print(
+            f"Status failed to set. Status code: {response.status_code}, {response.text}"
+        )
+
+
+def set_status(
+    user_id: str = MM_USER_ID,
+    status: str = "online",
+    dnd_end_time: int | None = None
+):
+    """
+    Function to set Mattermost status
+    """
+
+    url = f"{MM_URL}/api/v4/users/{user_id}/status"
+    payload = {
+        "user_id": user_id,
+        "status": status,
+        "dnd_end_time": dnd_end_time,
+    }
+    send_request(url, payload)
+
+
+def set_custom_status(
+    user_id: str = MM_USER_ID,
+    emoji: str = "house",
+    text: str = "Working from home",
+    duration: str | None = None,
+    expires_at: str | None = None
+):
+    """
+    Function to set custom Mattermost status
+    """
+
+    url = f"{MM_URL}/api/v4/users/{user_id}/status/custom"
+    payload = {
+        "user_id": user_id,
+        "emoji": emoji,
+        "text": text,
+        "duration": duration,
+        "expires_at": expires_at
+    }
+    send_request(url, payload)
+
+
+if __name__ == "__main__":
+    set_status()
+    set_custom_status()
